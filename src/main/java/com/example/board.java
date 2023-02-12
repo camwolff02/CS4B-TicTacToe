686cabb5f2eca9c698cac25b1978b63f7db1e7e9
Cam <camwolff02@gmail.com>
2023-02-12 14:06:01 -0800

    added tictactoe functionality
    adds ability to play tictactoe game, meets most game board deliverables. Still bugs on changing images, player image place is switched from icons, player turn image not showing.

--- src/main/java/com/example/board.java
+++ src/main/java/com/example/board.java
@@ -8,6 +8,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 
@@ -22,118 +26,268 @@ public class board implements Initializable {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    
     @FXML
     private Button button1;
-
     @FXML
     private Button button2;
-
     @FXML
     private Button button3;
-
     @FXML
     private Button button4;
-
     @FXML
     private Button button5;
-
     @FXML
     private Button button6;
-
     @FXML
     private Button button7;
-
     @FXML
     private Button button8;
-
     @FXML
     private Button button9;
+   
+    @FXML
+    private ImageView playerXpfp;
+    @FXML
+    private ImageView playerOpfp;
+    @FXML
+    private ImageView gameStateImage;
 
     @FXML
-    private Label winnerText;
+    private ImageView image1;
+    @FXML
+    private ImageView image2;
+    @FXML
+    private ImageView image3;
+    @FXML
+    private ImageView image4;
+    @FXML
+    private ImageView image5;
+    @FXML
+    private ImageView image6;
+    @FXML
+    private ImageView image7;
+    @FXML
+    private ImageView image8;
+    @FXML
+    private ImageView image9;
 
-    private int playerTurn = 0;
+    @FXML
+    private Label scoreText;
+    @FXML
+    private Label gameStateText;
 
-    ArrayList<Button> buttons;
+    private ArrayList<Button> buttons;
+    private ArrayList<ImageView> images;
 
+    private int xBoardMask = 0b000_000_000;
+    private int oBoardMask = 0b000_000_000;
+    private int winningBoardMasks[];
+
+    private boolean playerXTurn = true;
+    private boolean imageMode = false;
+
+    private Image playerXImage;
+    private Image playerOImage;
+
+    private int xWinCount = 0;
+    private int oWinCount = 0;
+    private int tieCount  = 0;
+    private int moveCount = 0;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         buttons = new ArrayList<>(Arrays.asList(button1,button2,button3,button4,button5,button6,button7,button8,button9));
+        images  = new ArrayList<>(Arrays.asList(image1,image2,image3,image4,image5,image6,image7,image8,image9));
+
+        playerXImage = new Image("file:src/images/mittens.jpg");
+        playerOImage = new Image("file:src/images/soyjack.png");
+
+        gameStateImage = new ImageView(playerXImage);
+        playerXpfp = new ImageView(playerXImage);
+        playerOpfp = new ImageView(playerOImage);
+
+        winningBoardMasks = new int[] {
+            0b111_000_000,  // top row win
+            0b000_111_000,  // middle row win
+            0b000_000_111,  // bottom row win
+            0b100_100_100,  // left column win
+            0b010_010_010,  // middle column win
+            0b001_001_001,  // right column win
+            0b001_010_100,  // forward slash win
+            0b100_010_001   // backward slash win
+        };
 
         buttons.forEach(button ->{
             setupButton(button);
-            button.setFocusTraversable(false);
+            resetButton(button);
         });
     }
 
     @FXML
     void restartGame(ActionEvent event) {
         buttons.forEach(this::resetButton);
-        winnerText.setText("Tic-Tac-Toe");
+        gameStateText.setText("Player Turn: X");
+        updateScoreText();
+        
+        hideAllImages(true);
+        gameStateImage.setImage(playerXImage);
+        
+        playerXTurn = true;
+        moveCount   = 0;
+        xBoardMask  = 0;
+        oBoardMask  = 0;
+    }
+
+    @FXML
+    void showSymbolOnHover(MouseEvent event) {
+        String buttonID = ((Button)event.getSource()).getId();
+        int index = Integer.parseInt(String.valueOf(buttonID.charAt(buttonID.length()-1)))-1;
+
+        if (!buttons.get(index).isFocusTraversable()) return;
+        
+        if (playerXTurn) 
+            buttons.get(index).setText("X");
+        else 
+            buttons.get(index).setText("O");
+    }
+
+    @FXML
+    void hideSymbolOnHover(MouseEvent event) {
+        String buttonID = ((Button)event.getSource()).getId();
+        int index = Integer.parseInt(String.valueOf(buttonID.charAt(buttonID.length()-1)))-1;
+
+        if (buttons.get(index).isFocusTraversable()) 
+            buttons.get(index).setText("");
+    }
+
+    @FXML
+    void changeDisplayMode(ActionEvent event) {
+        if (imageMode) {  // turn off image mode
+            hideAllImages(false);
+            gameStateImage.setVisible(false);
+
+            ((ToggleButton) event.getSource()).setText("Toggle Image Mode");
+        }
+        else {  // turn on image mode    
+            for (var image : images) 
+                if (image.getImage() != null)
+                    image.setVisible(true);
+            
+            gameStateImage.setVisible(true);
+
+            ((ToggleButton) event.getSource()).setText("Toggle Classic Mode");
+        }
+
+        imageMode = !imageMode;
+    }
+
+    public void updateScoreText() {
+        scoreText.setText(String.format("%d - %d - %d", xWinCount, tieCount, oWinCount));
     }
 
     public void resetButton(Button button){
-        button.setDisable(false);
+        button.setFocusTraversable(true);
+        button.setMouseTransparent(false);
         button.setText("");
     }
-    
 
-   
     public void backtoMenu(ActionEvent event) throws IOException{
-       
         root  = FXMLLoader.load(getClass().getResource("menu.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-
     }
 
     private void setupButton(Button button) {
         button.setOnMouseClicked(mouseEvent -> {
             setPlayerSymbol(button);
-            button.setDisable(true);
-            checkIfGameIsOver();
+            button.setMouseTransparent(true);
+            button.setFocusTraversable(false);        
+        
+            moveCount++;
+        
+            checkGameOver();
         });
     }
 
+    public void lockAllButtons() {
+        for (var button : buttons) {
+            button.setMouseTransparent(true);
+            button.setFocusTraversable(false);   
+        }
+    }
+
+    public void hideAllImages(boolean clear) {
+        for (var image : images) {
+            image.setVisible(false);
+            if (clear) image.setImage(null);
+        }
+    }
+
     public void setPlayerSymbol(Button button){
-        if(playerTurn % 2 == 0){
+        String buttonID = button.getId();
+        int index = Integer.parseInt(String.valueOf(buttonID.charAt(buttonID.length()-1)))-1;
+
+        if (playerXTurn) {
             button.setText("X");
-            playerTurn = 1;
-        } else{
+            gameStateText.setText("Player Turn: O");
+            gameStateImage.setImage(playerOImage);
+
+            xBoardMask += Math.pow(2, index);  
+
+            images.get(index).setImage(playerXImage);
+
+            if (imageMode) 
+                images.get(index).setVisible(true);  
+        }
+        else { 
             button.setText("O");
-            playerTurn = 0;
+            gameStateText.setText("Player Turn: X");
+            gameStateImage.setImage(playerXImage);
+
+            oBoardMask += Math.pow(2, index);
+
+            images.get(index).setImage(playerOImage);
+
+            if (imageMode) 
+                images.get(index).setVisible(true);
         }
+
+        playerXTurn = !playerXTurn;
     }
 
-    public void checkIfGameIsOver(){
-        int count =0;
-        for(Button button : buttons)
-        {
-            boolean checkbutton = button.isDisable();
-            if(checkbutton == true)
-            {
-                count++;
+    public void checkGameOver() {        
+        for (var boardMask : winningBoardMasks) {
+            if ((xBoardMask & boardMask) == boardMask) {
+                xWinCount++;
+                gameStateText.setText("Player X Wins!");
+                updateScoreText();
+                lockAllButtons();
+
+                if (imageMode) 
+                    gameStateImage.setImage(playerXImage);
+
+                return;
             }
+            if ((oBoardMask & boardMask) == boardMask) {
+                oWinCount++;
+                gameStateText.setText("Player O Wins!");
+                updateScoreText();  
+                lockAllButtons();  
 
+                if (imageMode) 
+                    gameStateImage.setImage(playerOImage);
+                
+                return;
+            }
         }
-        if(count ==9)
-        {
-            
-            winnerText.setText("game over");
-        }
- 
-
-            // //X winner
-            // if (line.equals("XXX")) {
-            //     winnerText.setText("X won!");
-            // }
-
-            // //O winner
-            // else if (line.equals("OOO")) {
-            //     winnerText.setText("O won!");
-            // }
+        
+        if (moveCount >= 9) {
+            tieCount++;
+            updateScoreText();
+            gameStateText.setText("Tie Game");
         }
     }
+}
