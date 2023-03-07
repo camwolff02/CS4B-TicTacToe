3e57492943dbb3019cfdb08c32d4d811b39920e0
Peijun Zhao <pzhao1@saddleback.edu>
2023-03-06 17:27:52 -0800

    Adding some committing to Server class

--- Server.java
+++ Server.java
@@ -3,22 +3,26 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server{
+    // this is for listening for incoming connections or clients 
+    // to creat a socket object for conmunication 
     private ServerSocket serverSocket;
 
-    //a construter method
+    //a construtor method
     public Server(ServerSocket serverSocket){
         this.serverSocket = serverSocket;
     }
 
-    // a run method that will start the server
+    // a run method that will start the server and keeping the server running
     public void startServer(){
         try{
-
+            // the server will be constantly running untill the server socket is closed
             while (!serverSocket.isClosed()){
+                // a blocking method for halting the program untill a client has conected
                 Socket socket = serverSocket.accept();
                 System.out.println("A new client has connected!");
-                ClientHandler clienHandler = new ClientHandler(socket);
 
+                // a class that will be responsible for the communication with the client and have a runnable interface
+                ClientHandler clienHandler = new ClientHandler(socket);
                 Thread thread = new Thread(clienHandler);
                 thread.start();
             }
@@ -27,7 +31,7 @@ public class Server{
         }
     }
 
-    // a method that will close the server socket
+    // a method that will close the server socket if an error has occurs
     public void closeServerSocket(){
         // make sure the server socket is not pointed to a null
         try{
@@ -39,9 +43,9 @@ public class Server{
         }
     }
 
-    // here is the main method
+    // here is the main method to run
     public static void main(String[] args) throws IOException{
-        // created the server sovket with a port
+        // created the server socket with a port
         ServerSocket serverSocket = new ServerSocket(1234);
         // created the server object
         Server server = new Server(serverSocket);
