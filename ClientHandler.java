4c4dbac46146093802adef31111e8e757fb96d66
Peijun Zhao <pzhao1@saddleback.edu>
2023-03-06 17:39:15 -0800

    Adding cimmits on ClientHander class

--- ClientHandler.java
+++ ClientHandler.java
@@ -7,13 +7,15 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 // The sever start method will run this client handler class so there is no main here
+// implements Runnable here so the instances will be executed by a separate thread(override the run method) 
 public class ClientHandler implements Runnable{
 
+    // this array list is to keep track of all the clients
     public static ArrayList<ClientHandler> clientHandler = new ArrayList<>();
-    private Socket socket;
-    private BufferedReader bufferedReader;
-    private BufferedWriter bufferedWriter;
-    private String clientInfo;           // used for identify for whitch clinet
+    private Socket socket;                  // used for establish a connection between the client and server
+    private BufferedReader bufferedReader;  // used for reading message that is sent from the client
+    private BufferedWriter bufferedWriter;  // used for sending message to other client from a client
+    private String clientInfo;              // used for identify for whitch clinet
 
     // a construter method
     public ClientHandler(Socket socket){
@@ -23,7 +25,7 @@ public class ClientHandler implements Runnable{
             this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             this.clientInfo = bufferedReader.readLine();
 
-            // notify the other client when a new client as joint
+            // add the client to the array list and notify the other client when a new client as joint
             clientHandler.add(this);
             broadcastMessage("Sever: " + clientInfo + " has enterd the room");
 
@@ -32,7 +34,7 @@ public class ClientHandler implements Runnable{
         }        
     }
 
-    // a method that listens for the message
+    // a method that listens for the message using a separate thread
     @Override
     public void run(){
         String clientMessage;
