9cd92a240d84c07cfda6a4a3c06ce62c3631addf
andrewcesario1 <63276479+andrewcesario1@users.noreply.github.com>
2023-05-22 19:26:36 -0700

    Update JoinQueueTask.java

--- TicTacToeUI/src/main/java/com/example/JoinQueueTask.java
+++ TicTacToeUI/src/main/java/com/example/JoinQueueTask.java
@@ -55,6 +55,7 @@ public class JoinQueueTask extends Task<Void> {
                     });
 
                     c.subscribeToChannel(boardID);
+                    c.unsubscribeFromChannel("join");
                     break;
                 }
             }
